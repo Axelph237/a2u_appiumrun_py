c17a404f2da8fd9ac675a2e192c5a4db876d114b
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-07-03 11:44:55 -0700

    Added functions to retrieve test script parameters at server initialization.

--- a2u_appiumrun/__init__.py
+++ a2u_appiumrun/__init__.py
@@ -1,29 +1,51 @@
 import os
+import importlib
 
 
-# Takes the filename and changes it as the following:
-# file_name -> 'File Name'
 def beautify_filename(name):
     return name.removesuffix('.py').replace('_', ' ').title()
 
 
-# Get the directory of the current script
-script_directory = os.path.dirname(os.path.abspath(__file__))
+def import_modules_from_directory(directory):
+    modules = []
 
-# Define a relative path to a file
-relative_path = 'tests\\'
+    for filename in os.listdir(directory):
+        if filename.endswith('.py') and filename != '__init__.py':
+            # Remove suffix and create path
+            module_name = filename[:-3]
+            module_path = f'tests.{module_name}'
+            # Import script as a module
+            module = importlib.import_module(module_path)
+            modules.append(module)
+    return modules
 
-# Construct the full path
-directory = os.path.join(script_directory, relative_path)
 
-print("File directory:")
-print(directory)
+def call_function_from_module(module, function_name):
+    func = getattr(module, function_name, None)
+    if callable(func):
+        func()
 
-print()
-print("Scripts:")
-# List all files in the directory and subdirectories
-scripts = []
-for root, dirs, filenames in os.walk(directory):
-    for filename in filenames:
-        print(beautify_filename(filename))
 
+def get_parameters_from_module(module):
+    return getattr(module, 'input_parameters', None)
+
+
+def main():
+    tests = []
+
+    tests_directory = os.path.join(os.getcwd(), 'tests')
+    modules = import_modules_from_directory(tests_directory)
+    test_id = 0
+    for module in modules:
+        tests.append(dict(
+            test=module.__name__.removeprefix('tests.'),
+            id=test_id,
+            params=get_parameters_from_module(module),
+        ))
+        test_id += 1
+
+    print(tests)
+
+
+if __name__ == '__main__':
+    main()

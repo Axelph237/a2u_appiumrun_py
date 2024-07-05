e1b1c2e8238bae91a13a651498d72236d3824fcf
CandyCaneCazoo <aiden.j.king004@gmail.com>
2024-07-05 09:23:52 -0700

    Created module to handle all tests and their parameter definitions.

--- a2u_appiumrun/apps.py
+++ a2u_appiumrun/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 import logging
 import os
 import importlib
+import a2u_appiumrun.data_handlers.test_handler as th
 
 logger = logging.getLogger(__name__)
 
@@ -10,57 +11,7 @@ class AppiumRunConfig(AppConfig):
     name = 'a2u_appiumrun'
 
     def ready(self):
-        print("StartupConfig is ready")
         # Add your initialization code here
-        self.run_startup_tasks()
-
-    def run_startup_tasks(self):
-        # Example startup task
-        print("Running startup tasks...")
-        # Add your startup tasks here
-        print("Hello from startup task!")
-
-
-def beautify_filename(name):
-    return name.removesuffix('.py').replace('_', ' ').title()
-
-
-def import_modules_from_directory(directory):
-    modules = []
-
-    for filename in os.listdir(directory):
-        if filename.endswith('.py') and filename != '__init__.py':
-            # Remove suffix and create path
-            module_name = filename[:-3]
-            module_path = f'a2u_appiumrun.tests.{module_name}'
-            # Import script as a module
-            module = importlib.import_module(module_path)
-            modules.append(module)
-    return modules
-
-
-def call_function_from_module(module, function_name):
-    func = getattr(module, function_name, None)
-    if callable(func):
-        func()
-
-
-def get_parameters_from_module(module):
-    return getattr(module, 'input_parameters', None)
-
-
-def collect_test_definitions():
-    tests = []
-
-    tests_directory = os.path.join(os.getcwd(), 'a2u_appiumrun', 'tests')
-    modules = import_modules_from_directory(tests_directory)
-    test_id = 0
-    for module in modules:
-        tests.append(dict(
-            test=module.__name__.removeprefix('tests.'),
-            id=test_id,
-            params=get_parameters_from_module(module),
-        ))
-        test_id += 1
-
-    print(tests)
+        th.init('C:\\Users\\aiden\\PycharmProjects\\a2u_appiumrun_py\\a2u_appiumrun\\tests')
+        print("\nGathering modules...")
+        print(str(th.modules))
---
+++ a2u_appiumrun/data_handlers/test_handler.py
@@ -0,0 +1,94 @@
+import os
+import importlib
+
+tests_directory = None
+modules = []
+definitions = []
+initialized = False
+
+
+# Used to initialize the module's values
+# Arguments:
+#   directory: the path to import test modules from
+def init(directory):
+    global tests_directory, initialized
+
+    initialized = True
+
+    tests_directory = directory
+    load_tests()
+
+
+# Loads all test modules found in the tests_directory
+def load_tests():
+    global modules, definitions
+
+    if not initialized:
+        raise ValueError(f"Tests_directory {tests_directory} is None. Module has not been initialized.")
+        return None
+
+    definitions.clear()
+    modules.clear()
+
+    modules = import_modules(tests_directory, 'a2u_appiumrun.tests')
+    create_definitions()
+
+
+# Takes all imported modules and creates a dictionary definition for them
+# Dictionary contains the following fields: file_name, test_id, params
+def create_definitions():
+    test_id = 0
+    for module in modules:
+        definitions.append(dict(
+            file_name=module.__name__.removeprefix('a2u_appiumrun.tests.'),
+            test_id=test_id,
+            params=call_function_from_module(module, 'get_parameters'),
+        ))
+        test_id += 1
+
+
+# Gets the definition of a test module at the specified index
+def get_definition(test_id):
+    if test_id < len(definitions):
+        return definitions[test_id]
+    else:
+        return None
+
+
+#  Runs the test at the index with test_id
+def run_test(test_id):
+    if test_id > len(definitions):
+        raise IndexError(f'Provided test_id ({test_id}) is out of range.')
+        return
+
+    main_func = getattr(modules[test_id], 'main', None)
+    if callable(main_func):
+        return main_func(definitions[test_id].params)
+
+
+# Imports python modules from the specified directory, given the package name
+# Arguments:
+#   directory: the path location of the modules
+#   package_name: the relative package name, i.e. 'foo.bar'
+def import_modules(directory, package_name):
+    global modules
+
+    modules = []
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.py') and filename != '__init__.py':
+            # Remove suffix and create path
+            module_name = filename[:-3]
+            module_path = f'{package_name}.{module_name}'
+            # Import script as a module
+            module = importlib.import_module(module_path)
+            modules.append(module)
+
+    return modules
+
+
+# Given a module and a function name as a String, runs a function and returns its value
+def call_function_from_module(module, function_name):
+    func = getattr(module, function_name, None)
+    if callable(func):
+        return func()

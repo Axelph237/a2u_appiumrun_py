2604ea90c2038c3524063c7d56dcb012cff1460f
CandyCaneCazoo <58153713+Axelph237@users.noreply.github.com>
2024-07-24 14:01:36 -0700

    Created check scripts to help test for errors in null definitions

---
+++ a2u_appiumrun/scripts/no_def_check.py
@@ -0,0 +1,45 @@
+import sys
+import unittest
+import json
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='emulator-5554',
+    appPackage='com.android.settings',
+    appActivity='.Settings',
+    ensureWebviewsHavePages='true',
+    nativeWebScreenshot='true',
+    newCommandTimeout=3600,
+    connectHardwareKeyboard='true',
+    language='en',
+    locale='US'
+)
+
+appium_server_url = 'http://localhost:4723'
+
+
+class TestAppium(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+
+    def tearDown(self) -> None:
+        if self.driver:
+            self.driver.quit()
+
+    def test_find_battery(self) -> None:
+        el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
+        el.click()
+
+
+def main():
+    # run unittest from Appium test class
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestAppium)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == '__main__':
+    main()
